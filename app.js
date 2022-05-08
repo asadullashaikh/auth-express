20c1d4bc1ce0d672f9ab0dafc3c6d7ce94405a1e
asadullashaikh <77007576+asadullashaikh@users.noreply.github.com>
2022-05-08 23:55:08 +0530

    make it better

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -18,25 +21,8 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
-
-// cookies
-app.get('/set-cookies', (req, res) => {
-
-  res.cookie('newUser', false)
-  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, secure: true });
-
-  res.send('you got cookies!')
-});
-
-app.get('/read-cookies', (req, res) => {
-
-  const cookie = req.cookies;
-  console.log(cookie);
-
-  res.json(cookie);
-
-})
