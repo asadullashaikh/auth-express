8762c4c667234d01c2826232b8faee206263de8d
asadullashaikh <77007576+asadullashaikh@users.noreply.github.com>
2022-05-08 17:55:00 +0530

    ll

--- models/User.js
+++ models/User.js
@@ -26,7 +26,7 @@ userSchema.pre('save', async function (next) {
 userSchema.statics.login = async function(email, password) {
     const user = await this.findOne({ email });
     if (user) {
-        const auth = await bcrypt.compare(password, user._id)
+        const auth = await bcrypt.compare(password, user.password)
         if (auth) {
             return user;
         }
